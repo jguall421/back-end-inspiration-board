a54a21a80d99e09ce7d00c02f6782352ca82adce
Jesica Guallpa <guallpajesica65@gmail.com>
2026-01-05 17:45:45 -0600

    up to making relationships with cards

--- app/models/board.py
+++ app/models/board.py
@@ -1,24 +1,57 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
+from typing import Optional
 from ..db import db
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
   from .card import Card
 
-class Card(db.Model):
+
+class Board(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
     name: Mapped[str]
+    cards: Mapped[list["Card"]] = relationship(back_populates="board")
+ 
 
 
     def to_dict(self):
-        boardr_as_dict = {
+        board_as_dict = {
             "id": self.id,
+            "title": self.title,
             "name": self.name
         }
-        
-        return boardr_as_dict
-    
+     
+        return board_as_dict
+
     @classmethod
     def from_dict(cls, board_data):
-        new_board = cls(name=board_data["name"])
+        new_board = cls(title=board_data["title"],
+                       name=board_data["name"])
+
         return new_board
+
+# from sqlalchemy.orm import Mapped, mapped_column, relationship
+# from ..db import db
+# from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+#   from .card import Card
+
+# class Card(db.Model):
+#     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+#     name: Mapped[str]
+
+
+    # def to_dict(self):
+    #     boardr_as_dict = {
+    #         "id": self.id,
+    #         "name": self.name
+    #     }
+        
+    #     return boardr_as_dict
+    
+    # @classmethod
+    # def from_dict(cls, board_data):
+    #     new_board = cls(name=board_data["name"])
+    #     return new_board
   
---
+++ app/routes/board_routes.py
@@ -0,0 +1,77 @@
+from flask import Blueprint, make_response, abort, request, Response
+from app.models.board import Board
+from .route_utilities import validate_model
+from ..db import db
+
+bp = Blueprint("board_bp", __name__, url_prefix="/boards")
+@bp.post("")
+def create_board():
+    request_body = request.get_json()
+
+    try:
+        new_board = Board.from_dict(request_body)
+
+    except KeyError as error:
+        response = {"message": f"Invalid request: missing {error.args[0]}"}
+        abort(make_response(response, 400))
+    db.session.add(new_board)
+    db.session.commit()
+
+    return new_board.to_dict(), 201
+
+@bp.get("")
+def get_all_boards():
+    # query = db.select(Board).order_by(Board.id)
+    query = db.select(Board)
+    
+    title_param = request.args.get("title")
+    if title_param:
+    
+        query = query.where(Board.title.ilike(f"%{title_param}%"))
+
+
+    name_param = request.args.get("name")
+    if name_param:
+        # In case there are books with similar titles, we can also filter by description
+        query = query.where(Board.name.ilike(f"%{name_param}%"))
+
+    boards = db.session.scalars(query.order_by(Board.id))
+    # We could also write the line above as:
+
+    boards_response = []
+    for board in boards:
+        boards_response.append(board.to_dict())
+        
+    return boards_response
+
+
+@bp.get("/<board_id>")
+def get_one_board(board_id):
+    board = validate_model(Board, board_id)
+
+    return board.to_dict()
+
+@bp.put("/<board_id>")
+def update_board(board_id):
+    board = validate_model(Board, board_id)
+    request_body = request.get_json()
+
+    board.title = request_body["title"]
+    board.name = request_body["name"]
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json") # 204 No Content
+
+@bp.delete("/<board_id>")
+def delete_board(board_id):
+    board = validate_model(Board, board_id)
+    db.session.delete(board)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+
+
+
+
+
